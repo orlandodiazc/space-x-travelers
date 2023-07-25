ff3c6f8d7cb3adfb29c2fae69ac299f2f57d8e56
Orlando Diaz <orlandodiazconde@gmail.com>
2023-07-25 11:09:33 -0500

    Add loading spinner

--- src/pages/Missions/index.tsx
+++ src/pages/Missions/index.tsx
@@ -1,3 +1,4 @@
+import Spinner from "../../components/spinner"
 import {
   Table,
   TableBody,
@@ -10,30 +11,42 @@ import MissionFactory from "./mission"
 
 export default function Missions() {
   const { data, isError, isLoading } = useGetMissionsQuery()
-  if (isLoading) return <div>Loading...</div>
-  if (isError || !data) return <div>There was an error</div>
+
   return (
     <div className="min-h-screen items-center bg-missions-mobile bg-cover pb-8 pt-24 sm:pt-32 md:bg-missions-tablet lg:bg-missions-desktop lg:pt-40">
       <section className="container flex flex-col gap-4">
-        <h1 className="text-4xl md:text-5xl">Active Missions</h1>
-        <div className="rounded-lg bg-black/25 backdrop-blur-xl">
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead>Name</TableHead>
-                <TableHead>Description</TableHead>
-                <TableHead>Manufacturers</TableHead>
-                <TableHead className="text-center">Status</TableHead>
-                <TableHead></TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {data.map((mission) => (
-                <MissionFactory mission={mission} key={mission.mission_id} />
-              ))}
-            </TableBody>
-          </Table>
-        </div>
+        {isLoading ? (
+          <div className="mx-auto">
+            <Spinner />
+          </div>
+        ) : isError || !data ? (
+          <h1>Unable to fetch missions</h1>
+        ) : (
+          <>
+            <h1 className="text-4xl md:text-5xl">Active Missions</h1>
+            <div className="rounded-lg bg-black/25 backdrop-blur-xl">
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead>Name</TableHead>
+                    <TableHead>Description</TableHead>
+                    <TableHead>Manufacturers</TableHead>
+                    <TableHead className="text-center">Status</TableHead>
+                    <TableHead></TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  {data.map((mission) => (
+                    <MissionFactory
+                      mission={mission}
+                      key={mission.mission_id}
+                    />
+                  ))}
+                </TableBody>
+              </Table>
+            </div>
+          </>
+        )}
       </section>
     </div>
   )
--- src/pages/Rockets/index.tsx
+++ src/pages/Rockets/index.tsx
@@ -1,19 +1,28 @@
+import Spinner from "../../components/spinner"
 import { useGetRocketsQuery } from "../../redux/services/spacex"
 import RocketFactory from "./rocket"
 
 export default function Rockets() {
   const { data, isError, isLoading } = useGetRocketsQuery()
-  if (isLoading) return <div>Loading...</div>
-  if (isError || !data) return <div>There was an error</div>
   return (
-    <div className="grid min-h-screen items-center bg-rockets-mobile bg-cover pb-8 pt-24 sm:pt-28 md:bg-rockets-tablet lg:bg-rockets-desktop lg:pt-40">
+    <div className="min-h-screen items-center bg-rockets-mobile bg-cover pb-8 pt-24 sm:pt-28 md:bg-rockets-tablet lg:bg-rockets-desktop lg:pt-40">
       <section className="container flex flex-col gap-4">
-        <h1 className="text-4xl md:text-5xl">Active Rockets</h1>
-        <div className="grid gap-4">
-          {data.map((rocket) => (
-            <RocketFactory rocket={rocket} key={rocket.id} />
-          ))}
-        </div>
+        {isLoading ? (
+          <div className="mx-auto">
+            <Spinner />
+          </div>
+        ) : isError || !data ? (
+          <h1>Unable to fetch rockets</h1>
+        ) : (
+          <>
+            <h1 className="text-4xl md:text-5xl">Active Rockets</h1>
+            <div className="grid gap-4">
+              {data.map((rocket) => (
+                <RocketFactory rocket={rocket} key={rocket.id} />
+              ))}
+            </div>
+          </>
+        )}
       </section>
     </div>
   )
